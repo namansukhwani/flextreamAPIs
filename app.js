68ecdb6e48b9c0947a2a04893b34d74e3b0e8694
Naman Sukhwani <namansukhwani@gmail.com>
2021-03-29 18:58:51 +0530

    push to heroku

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/users', usersRouter); 
 app.use('/fetch',fetchRouter);
 
 // catch 404 and forward to error handler
